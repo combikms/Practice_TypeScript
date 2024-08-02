01e7100bb1189d55ffab8e572e781c7d0416c3f8
[combikms] <combikms@naver.com>
2024-08-02 19:00:01 +0900

    :sparkles: 고급 Narrowing

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-;
-const obj = {
-    plus(a, b) {
-        return a + b;
-    },
-    minus(a, b) {
-        return a - b;
+function foo(animal) {
+    if ('swim' in animal) {
+        console.log("This is fish.");
     }
+    else {
+        console.log("This is bird.");
+    }
+}
+const a1 = { swim: 'aaa' };
+const a2 = { fly: 'aaa' };
+foo(a1);
+foo(a2);
+function boo(x) {
+    if (x.wheels === '4') {
+        console.log('This is car.');
+    }
+    else {
+        console.log('This is bike.');
+    }
+}
+const c = {
+    wheels: '4',
+    color: 'white'
 };
-console.log(obj.plus(2, 3));
-console.log(obj.minus(6, 2));
+const b = {
+    wheels: '2',
+    color: 'white'
+};
+boo(c);
+boo(b);
--- index.ts
+++ index.ts
@@ -1,16 +1,46 @@
-interface Foo {
-    plus(a: number, b: number): number
-    minus(a: number, b: number): number
-};
+type Fish = { swim: string };
+type Bird = { fly: string };
 
-const obj: Foo = {
-    plus(a, b) {
-        return a + b
-    },
-    minus(a, b) {
-        return a - b
+function foo(animal: Fish | Bird) {
+    if ('swim' in animal) {
+        console.log("This is fish.");
+    } else {
+        console.log("This is bird.");
     }
 }
 
-console.log(obj.plus(2, 3))
-console.log(obj.minus(6, 2))
+const a1: Fish = { swim: 'aaa' };
+const a2: Bird = { fly: 'aaa' };
+
+foo(a1);
+foo(a2);
+
+
+type Car = {
+    wheels: '4'
+    color: string
+};
+
+type Bike = {
+    wheels: '2'
+    color: string
+};
+
+function boo(x: Car | Bike) {
+    if (x.wheels === '4') {
+        console.log('This is car.');
+    } else {
+        console.log('This is bike.');
+    }
+}
+
+const c: Car = {
+    wheels: '4',
+    color: 'white'
+};
+const b: Bike = {
+    wheels: '2',
+    color: 'white'
+};
+boo(c);
+boo(b);
